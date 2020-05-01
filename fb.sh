507d7c0d8922cd7efe44d59f0000830bdc8da710
Gantai <50599491+mr-gantai@users.noreply.github.com>
2020-05-01 23:49:48 +0700

    Create fb.sh

---
+++ fb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+lightgreen='\e[1;32m'
+
+clear
+sleep 1                                                                                                               echo -e $lightgreen "        welcome di ig force attack"
+echo "            created by zhusyren7 "
+echo " "
+echo " "
+echo -e $cyan  "masukan target nya disini :"
+read name.
+sleep 1
+echo -e $r "sedang melakukan hack..."
+sleep 2
+echo -e  $r "mohon tunggu sebentar "
+sleep 1
+sleep 1
+echo -e $lightgreen"[✓] attack fb  $name sukses"
+sleep 1
+echo "password nya adalah samahkkmd @#771"
